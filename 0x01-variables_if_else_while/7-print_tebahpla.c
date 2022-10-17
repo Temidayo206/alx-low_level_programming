9506fe2f217b3538ab22f78e241ba46da8264344
Temidayo206 <temidayo206@gmail.com>
2022-10-17 05:34:18 -0700

    The last digit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-*main-program entry point
-*Return:0 if no errors, non zero if errors
+* main-program entry point
+* Return:0 if no errors, non zero if errors
 */
 int main(void)
 {
-	char i;
-
-	for (i = 'z' ;  i > = 'a' ; i--)
+	char i;	
+ 
+	for (i = 'a';  i <= 'z'; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
