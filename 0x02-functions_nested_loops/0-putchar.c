361968a14c2861a09ec83b03b5014281cc07a4f6
Temidayo206 <temidayo206@gmail.com>
2022-10-18 05:12:38 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - print _putchar
- * is written by Temidayo206
- * Return: 0
- */
+* main - print _putchar
+* is written by Temidayo206
+* Return: 0
+*/
 
 int main(void)
 {
