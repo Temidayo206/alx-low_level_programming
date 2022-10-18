ba46a6336e338cd964fd860dff3afae731e68624
Temidayo206 <temidayo206@gmail.com>
2022-10-18 08:09:56 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,13 +10,10 @@ int print_last_digit(int a)
 {
 	int last_digit;
 
-	int last_digit;
-
-	if (last_digit < 0);
+	if (last_digit < 0)
 	{
 		last_digit = last_digit * -1;
 	}
 	_putchar(last_digit + '0');
 	return (last_digit);
-
 }
