5016a18c0780505f87eaf95a7508f8188245b061
Temidayo206 <temidayo206@gmail.com>
2022-10-20 00:36:54 -0700

    Multiple mains

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* times_table - Prints the 9 times table
+* times_table - Prints the 9 times table.
 *
-* Return: no return
+* Return: no return.
 */
 void times_table(void)
 {
@@ -27,7 +27,7 @@ void times_table(void)
 			_putchar((op / 10) + 48);
 			_putchar((op % 10) + 48);
 			}
-		}	
+		}
 		_putchar('\n');
 	}
 }
