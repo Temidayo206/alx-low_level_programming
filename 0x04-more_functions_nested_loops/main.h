78893f877666f7b5d3d9672054e23b311285112c
Temidayo206 <temidayo206@gmail.com>
2022-10-20 03:56:23 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: Temidayo206
+ * Desc: Header contains prototype for all functions used in 0x04. C - More functions, more nested loops
+ */
+
+int _putchar(char c);
+
+#endif
