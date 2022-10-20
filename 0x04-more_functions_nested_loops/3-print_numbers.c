3875989aa2b199bc35117ca56962b1e1782eba2a
Temidayo206 <temidayo206@gmail.com>
2022-10-20 14:57:15 -0700

    tasks

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
-* print_numbers - prints numbers between  0 to 9.
-* Return: no return.
-*/
-
-void print_numbers(void);
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
 {
 	int ch;
 
